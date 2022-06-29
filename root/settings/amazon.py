00a0f9c247c39c57e81d7826e60c667260c213c6
Motanya Nyabanga <francismotanya@outlook.com>
2022-06-29 20:35:14 +0300

    bucket

--- root/settings/amazon.py
+++ root/settings/amazon.py
@@ -4,10 +4,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'makueni-sa'
 
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_REGION = 
 
 # AWS_S3_HOST = 's3.{}.amazonaws.com'.format(AWS)
 AWS_S3_CUSTOM_DOMAIN = '{}.S3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
