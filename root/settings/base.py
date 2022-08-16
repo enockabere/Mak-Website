74728bf58600f7286a5bea1b44b58ad72f838942
Motanya Nyabanga <francismotanya@outlook.com>
2022-08-16 09:42:53 +0300

    identity name

--- root/settings/base.py
+++ root/settings/base.py
@@ -125,6 +125,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR , '../media/')
 
 django_heroku.settings(locals())
