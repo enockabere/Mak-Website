79ba8731d09da2e1d09b9486aee1f24445b4accf
Motanya Nyabanga <francismotanya@outlook.com>
2022-08-18 16:08:43 +0300

    Mds Message

--- about/admin.py
+++ about/admin.py
@@ -48,7 +48,7 @@ class MDsMessageAdmin(SummernoteModelAdmin):
     )
 
     list_display = [
-        'profile_photo',
+        'profile_image',
         'title',
         'name_of_md',
         'status',
