ee22408a4819445838b0cec51941f1997ecffdfc
Motanya Nyabanga <francismotanya@outlook.com>
2022-08-19 16:52:47 +0300

    production

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,15 @@
+
 from django.db import models
 
 # Create your models here.
 
-
+TYPE_CHOICES = [
+    (0, 'Inqury'),
+    (1, 'Suggestions'),
+    (2, 'Complaints')
+]
 class Feedback(models.Model):
-    type = models.CharField(max_length=200)
+    type = models.IntegerField(choices=TYPE_CHOICES)
     name = models.CharField(max_length=200)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
