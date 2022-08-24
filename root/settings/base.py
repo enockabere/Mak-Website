68fadd7db88dad33d645079553d968c884e9cbe6
Motanya Nyabanga <francismotanya@outlook.com>
2022-08-24 17:00:17 +0300

    Footer Updates

--- root/settings/base.py
+++ root/settings/base.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
@@ -122,10 +124,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
 )
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 # Base url to serve media files
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR , '../media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
 
 django_heroku.settings(locals())
