4253497782a17264642c042bb35a8b12c2a75239
Motanya Nyabanga <francismotanya@outlook.com>
2022-08-30 16:31:34 +0300

    db update

--- resources/urls.py
+++ resources/urls.py
@@ -3,9 +3,6 @@ from resources import views
 
 urlpatterns = [
     path('resource-center/category/<int:pk>', views.reports_view, name='publication'),
-    # path('resource-center/acts/', views.acts_view, name='acts'),
-    # path('resource-center/sand-harvesting-sites/', views.harvesting_view, name='harvesting'),
-    # path('resource-center/sand-dealers/', views.delers_view, name='dealers'),
     path('resource-center/faq/', views.faq_view, name='faq'),
     path('resource-center/terms-of-service/', views.terms, name='terms'),
     path('resource-center/privacy-policy/', views.privacy, name='privacy')
