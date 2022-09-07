b8af79b3d420022b02aab3b2b2970ee3f1392f45
Motanya Nyabanga <francismotanya@outlook.com>
2022-09-07 09:51:10 +0300

    careet update

--- root/settings/__init__.py
+++ root/settings/__init__.py
@@ -1,8 +1,8 @@
-from .base import *
-try:
-    from .local import *
-    live = False
-except ImportError:
-    live = True
-if live:
-    from .production import *
+# from .base import *
+# try:
+#     from .local import *
+#     live = False
+# except ImportError:
+#     live = True
+# if live:
+from .production import *
